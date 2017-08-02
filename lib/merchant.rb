4980ab8f57000d1195913ff9baf5c639944a0dcf
rspringer47 <rspringer47@gmail.com>
2017-08-02 16:15:23 -0600

    fixing rake errors

--- lib/merchant.rb
+++ lib/merchant.rb
@@ -24,7 +24,7 @@ class Merchant
 
   def customers
     invoices = @merch_repo.sales_engine.invoices.find_all_by_merchant_id(@id)
-    customers = invoices.map do |invoice|
+    invoices.map do |invoice|
       @merch_repo.sales_engine.customers.find_by_id(invoice.customer_id)
     end.uniq
   end
